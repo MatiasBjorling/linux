22c6d64a1f8f2e7ffa72d2a448b221ec0bc112d5
Matias Bjørling <m@bjorling.me>
2015-09-08 15:58:38 +0200

    bm_hb: fix copyright
    Update to match my e-mail address and make the copyright follow other
    copyright notices in the kernel.

--- drivers/lightnvm/bm_hb.c
+++ drivers/lightnvm/bm_hb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright: Matias Bjorling <mb@lightnvm.io>
+ * Copyright (C) 2015 Matias Bjorling <m@bjorling.me>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
@@ -9,6 +9,11 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139,
+ * USA.
  *
  * Implementation of a block manager for hybrid open-channel SSD.
  */
